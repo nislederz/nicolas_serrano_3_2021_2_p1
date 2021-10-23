76579ad84b1ec88dee52deb2078f6b59d396a984
nislederz <nislederz@gmail.com>
2021-10-23 11:34:14 -0500

    initial page

--- nicolas_serrano_3_2021_2_p1/lib/screens/wait_screen.dart
+++ nicolas_serrano_3_2021_2_p1/lib/screens/wait_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:nicolas_serrano_3_2021_2_p1/components/loader_component.dart';
 
-class AnimeListScreen extends StatelessWidget {
-  const AnimeListScreen({ Key? key }) : super(key: key);
+class WaitScreen extends StatelessWidget {
+  const WaitScreen({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LoaderComponent(text: 'hola mundo',),
+      body: LoaderComponent(text: 'Por favor espere...',),
     );
   }
 }
